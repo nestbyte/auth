433eb92ce6f3ff29eabc22a4261f32e37b339511
Aryansh Mahato <contact@aryansh.dev>
2023-08-03 22:10:39 +0530

    feat(health): add health endpoint GH-1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AuthModule } from './auth.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AuthModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth')
+    .setDescription('Auth Service')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8010);
 }
 bootstrap();
